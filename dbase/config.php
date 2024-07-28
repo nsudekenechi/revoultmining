895a2d2d4dbfcddbf05eb4a5d134823a2a54b33c
kenechi <nsudekenechi2@gmail.com>
2024-07-28 18:04:59 +0100

    updated config.php

--- dbase/config.php
+++ dbase/config.php
@@ -1,8 +1,17 @@
 <?php
-$username = "root";
-$password = "";
-$db = "Zenixmining";
-$host = "localhost";
+$env = "prod";
+if ($env == "prod") {
+    $db = "u779058360_zenixmining_db";
+    $username = "u779058360_zenixmining_us";
+    $password = "#Zenix1234";
+    $host = "localhost";
+} else {
+    $db = "zenixmining";
+    $username = "root";
+    $password = "";
+    $host = "localhost";
+}
+
 $conn = mysqli_connect($host, $username, $password, $db);
 if (!$conn) {
     echo "Couldn't connect to DB";
